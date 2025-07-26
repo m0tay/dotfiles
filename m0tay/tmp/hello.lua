5d51ae02964c498b5bc04cf2b51b63aedbf6bab5
Douglas Lobo <douglascnlobo@icloud.com>
2025-07-26 23:33:57 +0100

    wip

--- m0tay/tmp/hello.lua
+++
@@ -1,8 +0,0 @@
-local function h(age, name)
-	return {
-		foo = "bar",
-		baz = "agosto",
-	}
-end
-
-h(2, "Hello")
--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -8,4 +8,6 @@ source "$HOMEBREW_PREFIX/share/zsh-autopair/autopair.zsh"
 
 source "$HOMEBREW_PREFIX/share/zsh-you-should-use/you-should-use.plugin.zsh"
 
-source "$HOME/.config/zsh/plugins/colored-man-pages.zsh"
+source "$HOMEBREW_PREFIX/share/zsh-colored-man-pages/colored-man-pages.plugin.zsh"
+
+source "$HOMEBREW_PREFIX/share/zsh-abbr/zsh-abbr.zsh"
