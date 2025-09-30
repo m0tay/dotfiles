b1155291d6d9de9992a8bac9a4b25ecd32bcd59e
Douglas Lobo <douglascnlobo@icloud.com>
2025-09-30 12:51:08 +0100

    trying out sylvan franklin settings

---
+++ scripts/time.sh
@@ -0,0 +1,18 @@
+#1/bin/bash
+
+CATEGORIES=(
+	"WORK"
+	"WASTED"
+	"UA"
+	"STOP"
+)
+
+selected=$(printf "%s\n" "${CATEGORIES[@]}" | sk --margin 10% --color="bw")
+
+if [[ "$selected" == "STOP" ]]; then
+	timew stop
+	tmux set -g status-right ""
+else
+	timew start "$selected"
+	tmux set -g status-right "$selected "
+fi
