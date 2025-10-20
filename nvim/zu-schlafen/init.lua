8285eb11c23b2eb50c5d5f33467cbb63d733e4cc
Douglas Lobo <douglascnlobo@icloud.com>
2025-10-20 19:49:36 +0100

    added markdown preview plugin

--- nvim/zu-schlafen/init.lua
+++ nvim/zu-schlafen/init.lua
@@ -20,6 +20,7 @@ vim.pack.add({
 	{ src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main" },
 	{ src = "https://github.com/mason-org/mason.nvim" },
 	{ src = "https://github.com/echaya/neowiki.nvim" },
+	{ src = "https://github.com/iamcco/markdown-preview.nvim" },
 })
 
 -- local ts = require("treesitter")
