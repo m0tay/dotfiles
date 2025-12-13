c07ca065cfc2f01e04074232d6ac6519a33a84cd
Douglas Lobo <douglascnlobo@icloud.com>
2025-12-13 23:59:38 +0000

    Update

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -2,7 +2,7 @@ source "$HOMEBREW_PREFIX/share/zsh-syntax-highlighting/zsh-syntax-highlighting.z
 
 source "$HOMEBREW_PREFIX/share/zsh-autopair/autopair.zsh"
 
-source "$HOMEBREW_PREFIX/share/zsh-you-should-use/you-should-use.plugin.zsh"
+# source "$HOMEBREW_PREFIX/share/zsh-you-should-use/you-should-use.plugin.zsh"
 
 source "$HOMEBREW_PREFIX/share/zsh-colored-man-pages/colored-man-pages.plugin.zsh"
 
