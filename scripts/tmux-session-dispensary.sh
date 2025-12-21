0f46238ba965adce098890605e3af16035dbe6f7
Douglas Lobo <douglascnlobo@icloud.com>
2025-12-21 03:08:28 +0000

    More depth in life is always good

--- scripts/tmux-session-dispensary.sh
+++ scripts/tmux-session-dispensary.sh
@@ -10,7 +10,7 @@ DIRS=(
 if [[ $# -eq 1 ]]; then
     selected=$1
 else
-    selected=$(fd . "${DIRS[@]}" --type=dir --max-depth=1 --full-path --base-directory $HOME \
+    selected=$(fd . "${DIRS[@]}" --type=dir --max-depth=2 --full-path --base-directory $HOME \
         | sed "s|^$HOME/||" \
         | sk --margin 10% --color="bw")
 
