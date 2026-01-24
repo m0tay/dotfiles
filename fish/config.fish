0a2e5e94b0bf6e8890fe02a46591be32f2a93845
Douglas Lobo <douglascnlobo@icloud.com>
2026-01-24 21:32:09 +0000

    trying out yazi

--- fish/config.fish
+++ fish/config.fish
@@ -27,6 +27,11 @@ function se --description "Fuzzy search"
   ls | fzf --reverse --margin=20% --query="$argv[1]"
 end
 
-function lf --description "List file"
-  yazi
+function y
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	command yazi $argv --cwd-file="$tmp"
+	if read -z cwd < "$tmp"; and [ "$cwd" != "$PWD" ]; and test -d "$cwd"
+		builtin cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
 end
