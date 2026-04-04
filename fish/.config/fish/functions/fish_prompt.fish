ac5d5f1854acfc93e3c18ff3dc8901b034c82943
Douglas Lobo <douglascnlobo@icloud.com>
2026-04-04 19:13:46 +0100

    add git branch in prompt

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -1,11 +1,16 @@
 function fish_prompt
-    set_color red
-    echo -n '['
-    set_color magenta
-    echo -n (prompt_pwd)
-    set_color red
-    echo -n '] '
-    set_color cyan
-    echo -n 'λ '
-    set_color normal
+  set_color red
+  echo -n '['
+  set_color magenta
+  echo -n (prompt_pwd)
+  set_color red
+  echo -n '] '
+  set -l git_info (fish_git_prompt "on %s ")  
+  if test -n "$git_info"  
+    set_color white  
+    echo -n $git_info  
+  end     
+  set_color cyan
+  echo -n 'λ '
+  set_color normal
 end
